bcc8f12ffee0450f4c983fd55eba795fc3245c8f
Kevin Moore <kevinmoore444@gmail.com>
2023-06-06 13:25:46 +0000

    second commit

--- src/main/java/SampleClassC.java
+++ src/main/java/SampleClassC.java
@@ -22,7 +22,7 @@ public class SampleClassC implements Comparable<SampleClassC>{
      * TODO: implement compareTo for this class
      *
      * CompareTo should return a negative value if this object's 'val' is less than another object's 'val',
-     * and positive otherwise. If 'val' is equal for both objects, return 0.
+     * and positive otherwise. If 'val' is equal for both objects, return 0
      *
      * @param other another object of type SampleClassC, which we are comparing to 'this' object.
      */
